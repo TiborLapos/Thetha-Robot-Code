7935e06601779ae4fbedaef8e6541ddc75ceee5e
Tibi <lapostibi@gmail.com>
2020-12-16 15:27:42 +0100

    Fixed some bugs from front and beck and master the speed need to be fixed but olso the comunication delay to..

--- Master-THE-HEAD/Master-THE-HEAD.ino
+++ Master-THE-HEAD/Master-THE-HEAD.ino
@@ -112,11 +112,11 @@ void go_stop() {
 }
 
 void go_front() {
-  going_speed(1, 60, 60, 35, 35);
+  going_speed(1, 38, 38, 38, 38);
 }
 
 void go_back() {
-  going_speed(-1, 129, 120, 35, 35);
+  going_speed(-1, 100, 100, 100, 100);
 }
 
 
